0662028b3b8290de7f643c62d0173699ef574251
Naeem Mannan <nmannan97@gmail.com>
2020-06-16 13:37:32 -0700

    adding files

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+import math
+
+def mult(a, b):
+    return(a*b)
+
+x = input("input first var")
+y = input("input second var")
+op = input("put an operation")
+
+if(op == '*'):
+    print(mult(x,y))
+
+    
